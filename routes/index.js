15bd88f73ac07e921b729eb2f7c841a47d9a624e
dominhhieu99 <dohieu19999@gmail.com>
2019-04-02 19:19:43 +0700

    a

--- routes/index.js
+++ routes/index.js
@@ -34,26 +34,57 @@ router.get('/', function(req, res, next) {
   });
 });
 
-router.post('/products/save-add', function(req, res, next){
-  // thu thập dữ liệu từ form
-  let {name, image, price,cate_id,detail} = req.body;
-  
-  // tạo ra đối tượng mới dạng Category
-  let model = new Product();
+router.post('/products/save-add', upload.single('image'),function(req, res, next){
+  var {name, price, detail} = req.body;
+  var image = req.file.path.replace('public', '');
+  var model = new Product();
 
-  // Cập nhật lại thông tin đối tượng vừa tạo với dữ liệu thu thập được từ form
   model.name = name;
   model.image = image;
   model.price = price;
-  model.cate_id = cate_id;
   model.detail = detail;
-  // Lưu đối tượng với csdl
-  model.save();
 
-  // điều hướng website về danh sách danh mục
-  res.redirect('/');
+  model.save(function(err){
+    res.redirect('/');
+  });
 });
 
+router.get('/products/edit/:cId', function(req, res, next){
+  var cId = req.params.cId;
+  Product.findOne({_id: cId}, function(err, data){
+    if(err){
+      res.send('id khong ton tai');
+    }
+    res.render('product/updateProduct', {products: data});
+  });
+
+});
+
+router.post('/products/save-edit', upload.single('image'), function(req, res, next){
+  
+  // neu khong upload anh => req.file == null
+  let {id, name, detail,price} = req.body;
+  Product.findOne({_id: id}, function(err, model){
+    if(err){
+      res.send('Id khong ton tai');
+    }
+
+    model.name = name;
+    model.detail = detail;
+    model.price = price;
+    if(req.file != null){
+      model.image = req.file.path.replace('public', '');
+    }
+    model.save(function(err){
+      if(err){
+        res.send('cap nhat khong thanh cong');
+      }
+
+      res.redirect('/');
+    })
+  })
+
+});
 
 router.get('/hieu', function(req, res, next){
   res.render('hieu');
@@ -77,7 +108,6 @@ router.post('/cates/save-add', upload.single('image'),function(req, res, next){
   model.save(function(err){
     res.redirect('/Category');
   });
-  
 });
 
 
